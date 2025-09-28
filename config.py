537ffab867faec05cd4679435ce03346b4e48593
SM-OAK <149765588+SM-OAK@users.noreply.github.com>
2025-09-28 14:33:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ API_HASH = environ.get("API_HASH", "9960806d290cf4170e43355fcc3687ac")
 BOT_TOKEN = environ.get("BOT_TOKEN", "8470211855:AAEcGaw7JlH4sHRuXS6L4BTC8rG9HkRJowQ")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6226520145').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "Svadvance2_bot") # without @
 PORT = environ.get("PORT", "8080")
 
@@ -40,7 +40,7 @@ CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 LINK_GENERATION_MODE = is_enabled(environ.get('LINK_GENERATION_MODE', "True"), True)
 
 # The user ID of the main management bot. Clones will ignore the above setting.
-OWNER_BOT_ID = int(environ.get("OWNER_BOT_ID", "0"))
+OWNER_BOT_ID = int(environ.get("OWNER_BOT_ID", "8470211855:AAEcGaw7JlH4sHRuXS6L4BTC8rG9HkRJowQ"))
 
 # Database Information
 DB_URI = environ.get("DB_URI", "mongodb+srv://mysimplestats:simplestats@cluster0.uelokbe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
