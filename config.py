4a311181e77f6aa252dc3279ebc926b8620fafbd
SM-OAK <149765588+SM-OAK@users.noreply.github.com>
2025-09-29 14:13:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "22321078"))
 API_HASH = environ.get("API_HASH", "9960806d290cf4170e43355fcc3687ac")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8470211855:AAEcGaw7JlH4sHRuXS6L4BTC8rG9HkRJowQ")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8470211855:AAEvBRHlApO0fksUZErxlcHzVGMY_YDjOMA")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6226520145').split()]
